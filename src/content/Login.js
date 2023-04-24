79553ce28ae0e00950df349287ab0533dec69a4f
4g1v3m3 <bebad.lp@gmail.com>
2023-04-24 20:20:28 +0600

    confirmation, menu, about

--- src/content/Login.js
+++ src/content/Login.js
@@ -2,8 +2,11 @@ import "./styles/Login.css"
 
 const Login = () => {
   return (
-    <div>
-      <h1>Under construction</h1>
+    <div className="Login">
+      <div className="under-construction-container">
+        <h1 className="construction-header">Under construction</h1>
+        <p className="construction-p">We apologize for any inconvenience caused by the missing part of our site that is currently under construction. We are working hard to complete this section as soon as possible, so please check back soon for updates. In the meantime, if you have any questions or concerns, please do not hesitate to contact us. We appreciate your patience and understanding.</p>
+      </div>
     </div>
   )
 }
